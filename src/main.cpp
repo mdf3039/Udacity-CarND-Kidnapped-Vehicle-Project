df4dc50f7c912c9660012ea4029bcb89a0fcb64d
mdf3039 <mdf3039@gmail.com>
2017-11-12 18:38:23 -0600

    13th commit - adjusted else statement

--- src/main.cpp
+++ src/main.cpp
@@ -80,14 +80,14 @@ int main()
 			pf.init(sense_x, sense_y, sense_theta, sigma_pos);
 			cout<<"Initialization Success."<<endl;
 		  }
-		  else {
+		  //else {
 			// Predict the vehicle's next state from previous (noiseless control) data.
 		  	double previous_velocity = std::stod(j[1]["previous_velocity"].get<std::string>());
 			double previous_yawrate = std::stod(j[1]["previous_yawrate"].get<std::string>());
 
 			pf.prediction(delta_t, sigma_pos, previous_velocity, previous_yawrate);
 			cout<<"Prediction Success."<<endl;
-		  }
+		  //}
 
 
 		  // receive noisy observation data from the simulator
