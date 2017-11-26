99c0b355e02603bd5af782d8ae7a6f0aeabb1fd7
mdf3039 <mdf3039@gmail.com>
2017-11-26 11:47:57 -0600

    60th - added print statements

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -173,6 +173,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
             else{
                 x2_dist = pow(subset_landmarks.landmark_list[hung_assignments[j]].x_f-mapped_observations[j].x,2);
                 y2_dist = pow(subset_landmarks.landmark_list[hung_assignments[j]].y_f-mapped_observations[j].y,2);
+                cout<<"Distances: "<<x2_dist<<","<<y2_dist<<endl;
             }
             particle_prob *= gauss_norm*exp(-1.0*(x2_dist/(2*x_var)+y2_dist/(2*y_var)));
             cout<<"Particle "<<i<<" probability: "<<particle_prob<<endl;
